4a3469bbbb3adaac6046982a2c92b273e28dd9a2
Jake <jfails@byu.edu>
2024-03-08 17:20:10 -0700

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import data from './CollegeBasketballTeams.json';
+
+const TeamData = data.teams;
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Header() {
+  return <h1>All Colleges in NCAA Basketball</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const { school, name, city, state } = this.props;
+    return (
+      <div className="team-card">
+        <h2>{school}</h2>
+        <p>Mascot: {name}</p>
+        <p>
+          Location: {city}, {state}
+        </p>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div className="team-list">
+      {TeamData.map((teamNum) => (
+        <Team {...teamNum} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <p>March Madness Teams</p>
+      <Header />
+      <TeamList />
     </div>
   );
 }
